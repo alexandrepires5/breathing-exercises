f3e3d96b93f54da403e5a72a9379875ff1f0e9aa
Alexandre Pires <apthoughts@protonmail.com>
2021-10-28 21:20:01 +0200

    Add intro text. Read user choice. Print out user choice.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello World")
+    println!("Welcome to breathing exercises in rust!");
+    println!("This program was made to help you guide through the breathing exercises that we have programmed!");
+    println!("You have 2 options:");
+    println!("1 - Box breathing");
+    println!("2 - Wim Hof breathing");
+    println!("Choose your option!: ");
+    let mut user_option = String::new();
+    io::stdin().read_line(&mut user_option).expect("Failed to read input!");
+    println!("Your choice was: {}", user_option);
 }
