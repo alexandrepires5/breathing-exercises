29c2152c5649a57234ce7440ceebd63fa978ea8e
Alexandre Pires <apthoughts@protonmail.com>
2021-10-28 21:21:50 +0200

    Create intro_text method. Move text to method.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::io;
 
-fn main() {
+fn intro_text() {
     println!("Welcome to breathing exercises in rust!");
     println!("This program was made to help you guide through the breathing exercises that we have programmed!");
     println!("You have 2 options:");
     println!("1 - Box breathing");
     println!("2 - Wim Hof breathing");
+}
+
+fn main() {
+    intro_text();
     println!("Choose your option!: ");
     let mut user_option = String::new();
     io::stdin().read_line(&mut user_option).expect("Failed to read input!");
