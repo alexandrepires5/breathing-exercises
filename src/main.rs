84bf2a10ffe3056db8d190ed8685ceed013f2b90
Alexandre Pires <apthoughts@protonmail.com>
2021-10-30 18:19:00 +0200

    Implement box breathing algorithm.

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,20 @@ fn box_breathing() {
     println!("First, put yourself comfortable!");
     println!("Make yourself comfortable! We can wait 10 seconds!");
     sleep(time::Duration::new(10, 0));
-    println!("Exhale all your air now!");
+    println!("Exhale all your air now. We can wait 6 seconds for this");
+    sleep(time::Duration::new(6, 0));
+    for i in 0..4 {
+        println!("Round {}", i + 1);
+        println!("Now, breathe in for 4 seconds!");
+        sleep(time::Duration::new(4, 0));
+        println!("Now, hold for 4 seconds!");
+        sleep(time::Duration::new(4, 0));
+        println!("Now, breathe out for 4 seconds!");
+        sleep(time::Duration::new(4, 0));
+        println!("Now, hold for 4 seconds!");
+        sleep(time::Duration::new(4, 0));
+    }
+    println!("All finished! Better now? :)");
 }
 
 fn main() {
