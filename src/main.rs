dc26a77e1747e8cb539c82fe2450f6127f1fd638
Alexandre Pires <apthoughts@protonmail.com>
2021-10-30 17:52:04 +0200

    Evaluate choices.

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,29 @@ fn read_user_choice() -> String {
     println!("Choose your option!: ");
     let mut user_option = String::new();
     io::stdin().read_line(&mut user_option).expect("Failed to read input!");
-    return user_option.trim().to_lowercase();
+    user_option.trim().to_lowercase()
 }
+// enum BreathingChoice {
+//     BoxBreathing(String),
+//     WimHofBreathing(String)
+// }
 
 fn main() {
     intro_text();
     let mut valid_choice = false;
-    while valid_choice != true{
+    while valid_choice != true {
         let user_option = read_user_choice();
-        if !user_option.eq("1") && !user_option.eq("2") {
-            println!("You inserted an invalid choice! Exiting");
+        if user_option.eq("1"){
+            valid_choice = true;
+            println!("Your choice was: BoxBreathing");
+            
+        }
+        else if !user_option.eq("1") && user_option.eq("2"){
+            println!("Wim hof breathing is not yet implemented!");
         }
         else {
-            println!("Your choice was: {}", user_option);
-            valid_choice = true;
+            println!("You inserted an invalid choice! Exiting");
         }
     }
-    
-    
 }
+
