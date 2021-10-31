12a5b5681da7deb993d51527de7e802389a0fca6
Alexandre Pires <apthoughts@protonmail.com>
2021-10-31 12:20:19 +0100

    Create file breathing_exercises. Move algorithm there.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@
 
 
 use std::io;
-use std::time;
-use std::thread::sleep;
+mod breathing_exercises;
 
 /// A display text for the user
 fn intro_text() {
@@ -23,32 +22,6 @@ fn read_user_choice() -> String {
     io::stdin().read_line(&mut user_option).expect("Failed to read input!");
     user_option.trim().to_lowercase()
 }
-// enum BreathingChoice {
-//     BoxBreathing(String),
-//     WimHofBreathing(String)
-// }
-
-/// Implementation of box breathing technique
-fn box_breathing() {
-    println!("Starting box breathing!");
-    println!("First, put yourself comfortable!");
-    println!("Make yourself comfortable! We can wait 10 seconds!");
-    sleep(time::Duration::new(10, 0));
-    println!("Exhale all your air now. We can wait 6 seconds for this");
-    sleep(time::Duration::new(6, 0));
-    for i in 0..4 {
-        println!("Round {}", i + 1);
-        println!("Now, breathe in for 4 seconds!");
-        sleep(time::Duration::new(4, 0));
-        println!("Now, hold for 4 seconds!");
-        sleep(time::Duration::new(4, 0));
-        println!("Now, breathe out for 4 seconds!");
-        sleep(time::Duration::new(4, 0));
-        println!("Now, hold for 4 seconds!");
-        sleep(time::Duration::new(4, 0));
-    }
-    println!("All finished! Better now? :)");
-}
 
 /// Main function. Gets user input, evaluates that its correct and an exercise
 fn main() {
@@ -59,7 +32,7 @@ fn main() {
         if user_option.eq("1"){
             valid_choice = true;
             println!("Your choice was: BoxBreathing");
-            box_breathing();
+            breathing_exercises::box_breathing();
         }
         else if !user_option.eq("1") && user_option.eq("2"){
             println!("Wim hof breathing is not yet implemented!");
