230d43d8f665247ca9f8fbbe409925f5ddd78d14
Alexandre Pires <apthoughts@protonmail.com>
2021-11-01 20:25:29 +0100

    Implement wim hof breathing.

--- src/breathing_exercises.rs
+++ src/breathing_exercises.rs
@@ -29,5 +29,34 @@ pub fn box_breathing() {
 }
 
 pub fn wim_hof_breathing() {
-    println!("Still implementing!!");
+    let holdout_time_in_seconds = [60, 90, 90];
+    println!("Starting Wim Hof breathing! 3 rounds");
+    println!("First, put yourself comfortable!");
+    println!("Make yourself comfortable! We can wait 10 seconds!");
+    sleep(time::Duration::new(10, 0));
+    println!("Ok, are you ready? Here we go!");
+    sleep(time::Duration::new(6, 0));
+    for i in 0..3 {
+        println!("Round {}", i + 1);
+        for j in 0..30 {
+            println!("Breath in!");
+            sleep(time::Duration::new(2, 0));
+            if j == 29 {
+                println!("Fully out!");
+                sleep(time::Duration::new(3, 0));
+            }
+            else {
+                println!("Breath out!");
+                sleep(time::Duration::new(2, 0));
+            }
+            
+        }
+        println!("Now, hold for {} seconds!", holdout_time_in_seconds[i]);
+        sleep(time::Duration::new(holdout_time_in_seconds[i], 0));
+        println!("Breathe in and hold for 15 seconds!");
+        sleep(time::Duration::new(15, 0));
+        println!("Breathe out...");
+        sleep(time::Duration::new(3, 0));
+    }
+    println!("All finished! Better now? :)");
 }
